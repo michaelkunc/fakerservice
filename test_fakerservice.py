b3f57ff9f6b854a5db46941299755d26d11259c9
michaelkunc <mjkunc@gmail.com>
2017-08-31 15:44:55 -0700

    further refactoring of the test suite

--- test_fakerservice.py
+++ test_fakerservice.py
@@ -4,6 +4,12 @@ import json
 from app import create_app
 
 
+endpoints = {'addresses': {'url': '/addresses/',
+                           'properties': ['city', 'state_prov', 'postal_code', 'country', 'street_address']},
+             'companies': {'url': '/companies/',
+                           'properties': ['company_name']}}
+
+
 class FakerServiceTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -17,26 +23,23 @@ class FakerServiceTestCase(unittest.TestCase):
         length = len(json.loads(response.get_data(as_text=True)))
         return {'response': response, 'keys': keys, 'length': length, 'data': data, 'status_code': response.status_code}
 
-    def test_addresses(self):
-        response = self.payload('/addresses/')
+    def endpoint_test(self, url, properties):
+        response = self.payload(url)
         self.assertEqual(response['status_code'], 200)
         self.assertEqual(100, response['length'])
         self.assertEqual(
-            set(['city', 'state_prov', 'postal_code', 'country', 'street_address']), response['keys'])
+            set(properties), response['keys'])
         self.assertTrue(response['data'][0] != response['data'][1])
-        response = self.payload('/addresses/?quantity=10')
+        response = self.payload("{}?quantity=10".format(url))
         self.assertEqual(10, response['length'])
+
+    def test_addresses(self):
+        self.endpoint_test(
+            endpoints['addresses']['url'], endpoints['addresses']['properties'])
 
     def test_companies(self):
-        response = self.payload('/companies/')
-        self.assertEqual(response['status_code'], 200)
-        self.assertEqual(100, response['length'])
-        self.assertEqual(
-            set(['company_name']), response['keys'])
-        self.assertTrue(response['data'][0] != response['data'][1])
-        response = self.payload('/companies/?quantity=10')
-        self.assertEqual(10, response['length'])
-
+        self.endpoint_test(endpoints['companies']['url'], endpoints[
+                           'companies']['properties'])
 
 if __name__ == '__main__':
     unittest.main()
