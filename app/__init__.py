069e0c4ccd3b538605446f2eb71099e73cf9e20b
michaelkunc <mjkunc@gmail.com>
2017-09-09 15:41:14 -0700

    create layout template and 404 template.

--- app/__init__.py
+++ app/__init__.py
@@ -56,5 +56,5 @@ def create_app(config_name):
 
     @app.errorhandler(404)
     def page_not_found(e):
-        return render_template('layout.html'), 404
+        return render_template('404.html'), 404
     return app
