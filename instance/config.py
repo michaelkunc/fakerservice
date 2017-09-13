d6d3752bd6bb124b68ffa56529a22c0ea6636c81
michaelkunc <mjkunc@gmail.com>
2017-09-12 17:08:30 -0700

    working on swagger config

--- instance/config.py
+++ instance/config.py
@@ -35,3 +35,20 @@ app_config = {
     'staging': StagingConfig,
     'production': ProductionConfig,
 }
+
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+            "title": "Fakerservice"
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
