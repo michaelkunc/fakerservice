51107d1183822b4bec3f0e59bfaaad8aa50f987d
michaelkunc <mjkunc@gmail.com>
2017-09-15 08:21:25 -0700

    simple parameterized test example complete.

--- test_refactor_tests.py
+++ test_refactor_tests.py
@@ -2,29 +2,23 @@ import unittest
 from app import create_app
 
 
-# class ParameterizedTestCase(unittest.TestCase):
-
-# def setUp(self):
-#     self.app = create_app(config_name='testing')
-#     self.client = self.app.test_client
-
-# def test_status_code(self):
-#     self.app = create_app(config_name='testing')
-#     self.client = self.app.test_client
-#     for i in (['/addresses/full_addresses', '/addresses/countries/']):
-#         response = self.client().get(i)
-#         with self.subtest(response=response.status_code):
-#             self.assertEqual(200, response)
-
 class StatusTest(unittest.TestCase):
+
+    test_map = {
+        'addresses': '/addresses/full_addresses/',
+        'countries': '/addresses/countries/'
+    }
 
     def test_status_code(self):
         """
-        Test that numbers between 0 and 5 are all even.
+        Test that endpoints return 200s
         """
-        for i in [0, 6]:
-            with self.subTest(i=i):
-                self.assertEqual(i % 2, 0)
+        for name, url in self.test_map.items():
+            self.app = create_app(config_name='testing')
+            self.client = self.app.test_client
+            url = self.client().get(url)
+            with self.subTest(name=name):
+                self.assertEqual(url.status_code, 200, name)
 
 if __name__ == '__main__':
     unittest.main()
